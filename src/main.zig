bef08f99ba0c324eb5805b4a0d0582f08145dd87
Oliwier Michalik <oliwier.michalik9@gmail.com>
2025-07-05 21:16:19 +0200

    feat: implement pretty print for AST based on visitors

--- src/main.zig
+++ src/main.zig
@@ -15,12 +15,17 @@ pub fn main() !void {
     const source =
         \\import "source.brr";
         \\import "another.brr" as another;
+        \\fn hello(arg1, arg2) {}
+        \\native "C" fn lorem(ipsum: int, dolor);
     ;
     var lexer: patapim.Lexer = .{ .source = source };
     var parser = patapim.Parser.init(allocator, &lexer);
     defer parser.deinit(true);
 
     const module = try parser.parseWholeSource();
-    var printer = patapim.ast.PrettyPrinter{};
+    var printer = patapim.ast.PrettyPrinter{
+        .writer = std.io.getStdOut().writer().any(),
+        .print_spans = false,
+    };
     try printer.accept(&parser.tree, module);
 }
